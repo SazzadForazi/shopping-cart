c2d517d0b8f7a8c63a27d4b9135d838d083ff88e
Sazzad Hossain <sazzad.csebubt@gmail.com>
2021-08-19 07:08:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 1219;
+}
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+
+    updateCaseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
+});
